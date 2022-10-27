923c9792cbb06d5e0baae012bd635f7e9d5fb4ae
William Höök-Specker <william@rhyme.se>
2022-10-27 15:58:46 +0200

    improves dropdown functionality and sets global anchor tag font size-weight

--- src/modules/menu.module/module.js
+++ src/modules/menu.module/module.js
@@ -8,40 +8,47 @@ var childToggle = document.querySelectorAll(
 );
 var overlay = document.querySelectorAll('.overlay');
 
+// Get element siblings
+const getElementSiblings = function (el) {
+  let siblings = [];
+
+  if (!el.parentNode) {
+    return siblings;
+  }
+
+  let sibling = el.parentNode.firstChild;
+  while (sibling) {
+    if (sibling.nodeType === 1 && sibling !== el) {
+      siblings.push(sibling);
+    }
+    sibling = sibling.nextSibling;
+  }
+  return siblings;
+};
+
 // Desktop menu
 
 if (menuParentItems) {
   Array.prototype.forEach.call(menuParentItems, function (el) {
     // Menu item variables
-
     var childToggle = el.querySelector('.menu__child-toggle');
 
-    // // Handles hover over
-
-    // el.addEventListener('mouseover', function () {
-    //   this.classList.add('menu__item--open');
-    //   this.querySelector('a').setAttribute('aria-expanded', 'true');
-    //   this.querySelector('button').setAttribute('aria-expanded', 'true');
-    // });
-
-    // // Handles hover out
-
-    // el.addEventListener('mouseout', function () {
-    //   document
-    //     .querySelector('.menu__item--open > a')
-    //     .setAttribute('aria-expanded', 'false');
-    //   document
-    //     .querySelector('.menu__item--open > button')
-    //     .setAttribute('aria-expanded', 'false');
-    //   document
-    //     .querySelector('.menu__item--open')
-    //     .classList.remove('menu__item--open');
-    // });
+    var getSiblings = function (element) {
+      var siblings = [];
+      var sibling = element.parentNode.firstChild;
+      while (sibling) {
+        if (sibling.nodeType === 1 && sibling !== element) {
+          siblings.push(sibling);
+        }
+        sibling = sibling.nextSibling;
+      }
+      return siblings;
+    };
 
     // Handles toggle of submenus
-
     childToggle.addEventListener('click', function () {
       var overlay = document.getElementById('overlay');
+      var parentSiblings = getSiblings(this.parentNode);
 
       if (this.parentNode.classList.contains('menu__item--open')) {
         this.parentNode.classList.remove('menu__item--open');
@@ -61,6 +68,18 @@ if (menuParentItems) {
         this.parentNode
           .querySelector('button')
           .setAttribute('aria-expanded', 'true');
+
+        // Close all open dropdowns except nav item clicked
+        parentSiblings.forEach(function (sibling) {
+          if (sibling.classList.contains('menu__item--open')) {
+            sibling.classList.remove('menu__item--open');
+            sibling.querySelector('a').setAttribute('aria-expanded', 'false');
+            sibling
+              .querySelector('button')
+              .setAttribute('aria-expanded', 'false');
+            // console.log(sibling);
+          }
+        });
 
         overlay.classList.add('show-overlay');
       }
