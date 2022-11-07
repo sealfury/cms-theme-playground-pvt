c8401cd43053c62ca4e0744c0b378a4c02840a44
William Höök-Specker <william@rhyme.se>
2022-11-07 17:23:25 +0100

    adds more styles and js to simple dropdown menu

---
+++ src/modules/simple-dropdown.module/module.js
@@ -0,0 +1,23 @@
+// Menu variables 
+
+var parentMenuItems = document.querySelectorAll('.top-menu--desktop .top-menu__item--has-submenu');
+var menuChildToggle = document.querySelectorAll('.top-menu--mobile .top-menu__child-toggle');
+
+if (parentMenuItems) {
+  parentMenuItems.forEach(function (item) {
+    var menuChildToggle = item.querySelector('.top-menu__child-toggle');
+    // TODO: add focus on siblings on mouseover (hover effect on both)
+
+    menuChildToggle.addEventListener('click', function () {
+      if (this.parentNode.classList.contains('top-menu__item--open')) {
+        this.parentNode.classList.remove('top-menu__item--open');
+        this.parentNode.querySelector('a').setAttribute('aria-expanded', 'false');
+        this.parentNode.querySelector('button').setAttribute('aria-expanded', 'false');
+      } else {
+        this.parentNode.classList.add('top-menu__item--open');
+        this.parentNode.querySelector('a').setAttribute('aria-expanded', 'true');
+        this.parentNode.querySelector('button').setAttribute('aria-expanded', 'true');
+      }
+    });
+  });
+}
