fb3724eecbbadda0c76470037e4e793f3d9fc146
William Höök-Specker <william@rhyme.se>
2022-11-08 11:41:32 +0100

    consolodates child toggle functionality

--- src/modules/simple-dropdown.module/module.js
+++ src/modules/simple-dropdown.module/module.js
@@ -7,7 +7,7 @@ var menuChildToggle = document.querySelectorAll(
   '.top-menu--mobile .top-menu__child-toggle'
 );
 
-// Document variables 
+// Document variables
 var mainContent = document.getElementById('main-content');
 var headerBottomRow = document.querySelector('.header__row-2');
 var headerLogo = document.querySelector('.header__logo');
@@ -17,54 +17,37 @@ if (parentMenuItems) {
   parentMenuItems.forEach(function (item) {
     var menuChildToggle = item.querySelector('.top-menu__child-toggle');
     var childToggleLink = item.querySelector('.top-menu__link--toggle');
+    var childToggles = Array.from([menuChildToggle, childToggleLink]);
 
-    menuChildToggle.addEventListener('click', function () {
-      if (this.parentNode.classList.contains('top-menu__item--open')) {
-        this.parentNode.classList.remove('top-menu__item--open');
-        this.parentNode
-          .querySelector('a')
-          .setAttribute('aria-expanded', 'false');
-        this.parentNode
-          .querySelector('button')
-          .setAttribute('aria-expanded', 'false');
-      } else {
-        this.parentNode.classList.add('top-menu__item--open');
-        this.parentNode
-          .querySelector('a')
-          .setAttribute('aria-expanded', 'true');
-        this.parentNode
-          .querySelector('button')
-          .setAttribute('aria-expanded', 'true');
-      }
-    });
-
-    childToggleLink.addEventListener('click', function () {
-      if (this.parentNode.classList.contains('top-menu__item--open')) {
-        this.parentNode.classList.remove('top-menu__item--open');
-        this.parentNode
-          .querySelector('a')
-          .setAttribute('aria-expanded', 'false');
-        this.parentNode
-          .querySelector('button')
-          .setAttribute('aria-expanded', 'false');
-      } else {
-        this.parentNode.classList.add('top-menu__item--open');
-        this.parentNode
-          .querySelector('a')
-          .setAttribute('aria-expanded', 'true');
-        this.parentNode
-          .querySelector('button')
-          .setAttribute('aria-expanded', 'true');
-      }
+    childToggles.forEach(function (toggle) {
+      toggle.addEventListener('click', function () {
+        if (this.parentNode.classList.contains('top-menu__item--open')) {
+          this.parentNode.classList.remove('top-menu__item--open');
+          this.parentNode
+            .querySelector('a')
+            .setAttribute('aria-expanded', 'false');
+          this.parentNode
+            .querySelector('button')
+            .setAttribute('aria-expanded', 'false');
+        } else {
+          this.parentNode.classList.add('top-menu__item--open');
+          this.parentNode
+            .querySelector('a')
+            .setAttribute('aria-expanded', 'true');
+          this.parentNode
+            .querySelector('button')
+            .setAttribute('aria-expanded', 'true');
+        }
+      });
     });
   });
 
   // Close open top menu dropdowns on click outside
   outsideElements.forEach(function (element) {
     element.addEventListener('click', function () {
-      parentMenuItems.forEach(function (item) {
-        if (item.classList.contains('top-menu__item--open')) {
-          item.classList.remove('top-menu__item--open');
+      parentMenuItems.forEach(function (p) {
+        if (p.classList.contains('top-menu__item--open')) {
+          p.classList.remove('top-menu__item--open');
         }
       });
     });
