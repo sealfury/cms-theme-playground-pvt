50844f212fa66ee436e5b54634b730d12c7232ba
William Höök-Specker <william@rhyme.se>
2022-12-30 17:23:47 +0100

    adds eyebrow breadcrumb & hides non active chapter content updates styles

--- src/modules/toc-side-menu.module/module.js
+++ src/modules/toc-side-menu.module/module.js
@@ -1,6 +1,17 @@
 var chapterLinks = document.querySelectorAll('.toc-sidebar__menu--link');
 var pathLinkParam = window.location.href.split('#')[1];
 
+var splitPath = window.location.href.split('/');
+var editorPathParams = splitPath.filter(
+  (param) => param == 'preview' || param == 'content'
+);
+
+if (!editorPathParams.length) {
+  console.log('Live Mode');
+} else {
+  console.log('Edit Mode');
+}
+
 chapterLinks.forEach(function (link) {
   if (link.getAttribute('data-link-params') == pathLinkParam) {
     link.classList.add('active-page');
@@ -14,14 +25,16 @@ var rowWrapper = contentContainer.querySelectorAll(
   '.row-fluid-wrapper.dnd-section'
 );
 
-rowWrapper.forEach(function (row) {
-  var anchorTag = row.querySelectorAll('a');
-  anchorTag.forEach(function (tag) {
-    if (
-      tag.getAttribute('data-hs-anchor') &&
-      tag.getAttribute('id') == pathLinkParam
-    ) {
-      row.classList.add('active');
-    }
+if (!editorPathParams.length) {
+  rowWrapper.forEach(function (row) {
+    var anchorTag = row.querySelectorAll('a');
+    anchorTag.forEach(function (tag) {
+      if (
+        tag.getAttribute('data-hs-anchor') &&
+        tag.getAttribute('id') !== pathLinkParam
+      ) {
+        row.classList.add('inactive');
+      }
+    });
   });
-});
+}
