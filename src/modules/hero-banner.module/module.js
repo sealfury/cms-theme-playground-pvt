1d2d23a75397cca0ddff6cd793ebe94c0f673e1d
William Höök-Specker <william@rhyme.se>
2023-03-13 15:31:26 +0100

    adds click-video-to-popup functionality to hero banner video

---
+++ src/modules/hero-banner.module/module.js
@@ -0,0 +1,65 @@
+var heroBannerVideo = document.querySelector('.hero-media-player__wrapper');
+var popupContainer = document.querySelector('.popup-container');
+var popupOverlay = document.querySelector('.popup-overlay');
+var iframeContainer = document.querySelector('.popup-iframe-container');
+var videoIframe = document.getElementById('popup-iframe');
+var closeButton = document.querySelector('.popup-close');
+
+
+// Use existing video iframe in modal instead of creating new
+function cloneIframe(parent, container) {
+  var iframe = parent.querySelector('iframe');
+  var clonedIframe = iframe.cloneNode(true);
+  clonedIframe.setAttribute('id', 'popup-iframe');
+  container.insertAdjacentElement('afterbegin', clonedIframe);
+}
+
+// Activate popup video modal on click anywhere in hero banner video
+function activatePopup() {
+  var popupElements = Array.from([
+    popupContainer,
+    popupOverlay,
+    iframeContainer,
+    closeButton
+  ]);
+  var closePopupClickables = Array.from([ closeButton, popupOverlay ]);
+
+  if (heroBannerVideo.getAttribute('data-has-popup') == "true") {
+    var clickToPopupLink = document.querySelector('.hero-media-player__popup');
+  
+    clickToPopupLink.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      popupElements.forEach(function (element) {
+        element.classList.add('show-popup');
+      });
+    });
+
+    // Close popup on click outside or on close icon
+    closePopupClickables.forEach(function (clickable) {
+      clickable.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        popupElements.forEach(function (element) {
+          if (element.classList.contains('show-popup')) {
+            element.classList.remove('show-popup');
+          }
+        });
+      });
+    });
+  }
+}
+
+if (heroBannerVideo.children.length) {
+  document.addEventListener('DOMContentLoaded', function () {
+    // Wait for hubspot video iframe to load before trying to clone
+    setTimeout(() => {
+      cloneIframe(heroBannerVideo, iframeContainer);
+    }, 100);
+
+    // Wait for successful cloning/placing of iframe before enabling popup features
+    setTimeout(() => {
+      activatePopup();
+    }, 200);
+  });
+}
