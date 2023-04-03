0517d4118b055b8248d22e3142ab08e49ca08510
William Höök-Specker <william@rhyme.se>
2023-04-03 14:59:23 +0200

    updates webhook for download newsletter email form

--- src/modules/newsletter-cta.module/module.js
+++ src/modules/newsletter-cta.module/module.js
@@ -58,7 +58,7 @@ function submitFormData(formElement) {
 
 window.addEventListener('load', function () {
   var formContainer = document.querySelector(
-    '#hs_form_target_newsletter-email-form'
+    '#hs_form_target_trial'
   );
   var form = formContainer.querySelector('form');
   var legalConsent = form.querySelector('.legal-consent-container');
